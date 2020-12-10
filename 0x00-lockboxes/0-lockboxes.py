f1e44a9fb4eb97d8660cca1d1df056f02244bb37
Samuel Pomeroy <1690@holbertonschool.com>
2020-12-10 00:19:25 +0000

    now handling 0 length of boxes, and keys beyond index range

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
+
+    if (len(boxes) == 0):
+        return True
+
     keys = set()
     new_keys = set(boxes[0])
 
@@ -10,15 +14,17 @@ def canUnlockAll(boxes):
         keys = keys | new_keys
         new_keys = set()
 
-        # two possible solutions, given that box 0 is always unlocked
-        if (keys == set(range(len(boxes))) or keys == set(
-                range(1, len(boxes)))):
+        # check keyring for intersection with solution: set { 1..<last box #> }
+        if (keys & set(range(1, len(boxes))) == set(range(1, len(boxes)))):
             return True
 
         # use current keyring to get a new round of keys
         for x in keys:
-            for key in boxes[x]:
-                if key not in keys:
-                    new_keys.add(key)
+            # index protection for keys that have no boxes
+            if (x < len(boxes)):
+                # open box, add keys
+                for y in boxes[x]:
+                    if y not in keys:
+                        new_keys.add(y)
 
     return False
