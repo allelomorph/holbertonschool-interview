e78ab16af8f552e504212b83a10523c46bae57f7
Samuel Pomeroy <1690@holbertonschool.com>
2021-01-06 20:51:57 +0000

    first attempt 0x03 t0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+0x03. Minimum Operations - dertimines minimum number of "copy all"/"paste"
+operations to produce `n` number characters starting from 1.
+"""
+
+
+def minOperations(n):
+
+    if (n < 1):
+        return 0
+
+    minOps = 0
+
+    # within the potential factors of n, aside from 1
+    for i in range(2, (n + 1)):
+        # for each factor found
+        while (n % i == 0):
+            # add that to the total operations
+            minOps += i
+            # refocus to consider the remainder unprinted
+            n /= i
+
+    return minOps
