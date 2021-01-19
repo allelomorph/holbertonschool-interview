6b3af85c3fe2d8507b6990df230d6ff08010ea2b
Samuel Pomeroy <1690@holbertonschool.com>
2021-01-19 08:21:26 +0000

    first attempt 0x05 t0 using recursion

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/* NULL size_t */
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -22,5 +25,7 @@ void free_listint(listint_t *head);
 
 /* 0-is_palindrome.c */
 int is_palindrome(listint_t **head);
+int is_palindrome_recursion(listint_t **head, listint_t *tail);
+
 
 #endif /* LISTS_H */
