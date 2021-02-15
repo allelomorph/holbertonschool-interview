dd45eb568dd4092485c4338ea756aa7a53b98651
Samuel Pomeroy <1690@holbertonschool.com>
2021-02-15 21:33:45 +0000

    first attempt 0x09 t0

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""0x09. UTF-8 Validation - task 0. UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """Validates UTF-8 encoding of series of bytes represented as a list of
+    integers.
+
+    This is a Pythonic way to automate what can be done manually by checking
+    the values in a byte array. UTF-8 characters can be 1-4 bytes wide, and
+    valid formatting is as follows:
+        U+n:        bytes:      byte prefixes:
+        0-7f          1         0xxxxxxx
+        80-7ff        2         110xxxxx, 10xxxxxx
+        800-ffff      3         1110xxxx, 10xxxxxx, 10xxxxxx
+        10000-10ffff  4         11110xxx, 10xxxxxx, 10xxxxxx, 10xxxxxx
+
+    Args:
+        data (list): list of integers representing one byte value each
+
+    Returns:
+        bool: True if encoding validated, False otherwise
+    """
+    if type(data) is not list and type(data[0]) is not int:
+        return False
+
+    # convert signed to unsigned for bytes(), only need to consider up to 256
+    # as these ints represent bytes for this exercise
+    data = [n + 256 if n < 0 else n for n in data]
+
+    try:
+        # bytes.decode() default args, included for clarity
+        bytes(data).decode(encoding='utf-8', errors='strict')
+    except (ValueError, UnicodeDecodeError):
+        return False
+
+    return True
