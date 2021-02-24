c8b3dc5535c0396f8f531f29d880763b44118495
Samuel Pomeroy <1690@holbertonschool.com>
2021-02-24 05:39:46 +0000

    optimized for single pass vs double

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -2,6 +2,8 @@
 
 /* fprintf */
 #include <stdio.h>
+/* true false */
+#include <stdbool.h>
 
 
 /**
@@ -16,39 +18,34 @@ static void slide_line_left(int *line, size_t size)
 {
 	int i, j;
 
-	/* first pass: compress all values to left */
-	for (i = 0; i < (int)size; i++)
+	i = 0;
+	while (i < (int)size)
 	{
-		/* step to next 0 */
-		if (line[i] == 0)
-		{
-			/* step to next non-0 */
-			for (j = i; j < (int)size; j++)
-			{
-				if (line[j] != 0)
-				{
-					line[i] = line[j];
-					line[j] = 0;
-					break;
-				}
-			}
-		}
+		/* find next unslid value */
+		for (j = i + 1; j < (int)size && line[j] == 0; j++)
+		{}
+		/* no more unslid found */
 		if (j == (int)size)
 			break;
-	}
 
-	/* second pass: combine values from left */
-	for (i = 0; i < (int)size - 1 && line[i] != 0; i++)
-	{
-		/* combine adjacent identical values */
-		if (line[i + 1] == line[i])
+		/* slide unslid value to left if space open */
+		if (line[i] == 0)
 		{
-			line[i] *= 2;
-			/* slide remaining values into place */
-			for (j = i + 1; j < (int)size - 1; j++)
-				line[j] = line[j + 1];
-			/* add 0 padding to right to replace value */
+			line[i] = line[j];
 			line[j] = 0;
+
+		}
+		/* value already in that position: combine? */
+		else
+		{
+			/* next non-0 value matches current */
+			if (line[j] == line[i])
+			{
+				line[i] *= 2;
+				line[j] = 0;
+			}
+			/* advance to next whether combined or incompatible */
+			i++;
 		}
 	}
 }
@@ -66,42 +63,38 @@ static void slide_line_right(int *line, size_t size)
 {
 	int i, j;
 
-	/* first pass: compress all values to right */
-	for (i = (int)size - 1; i > -1; i--)
+	i = (int)size - 1;
+	while (i >= 0)
 	{
-		/* step to next 0 */
+		/* find next unslid value */
+		for (j = i - 1; j >= 0 && line[j] == 0; j--)
+		{}
+		/* no more unslid found */
+		if (j == -1)
+			break;
+
+		/* slide unslid value to right if space open */
 		if (line[i] == 0)
 		{
-			/* step to next non-0 */
-			for (j = i; j > -1; j--)
-			{
-				if (line[j] != 0)
-				{
-					line[i] = line[j];
-					line[j] = 0;
-					break;
-				}
-			}
-		}
-		if (j == 0)
-			break;
-	}
-
-	/* second pass: combine values from right */
-	for (i = (int)size - 1; i > 0 && line[i] != 0; i--)
-	{
-		/* combine adjacent identical values */
-		if (line[i - 1] == line[i])
-		{
-			line[i] *= 2;
-			/* slide remaining values into place */
-			for (j = i - 1; j > 0; j--)
-				line[j] = line[j - 1];
-			/* add 0 padding to left to replace value */
+			line[i] = line[j];
 			line[j] = 0;
+
+		}
+		/* value already in that position: combine? */
+		else
+		{
+			/* next non-0 value matches current */
+			if (line[j] == line[i])
+			{
+				line[i] *= 2;
+				line[j] = 0;
+			}
+			/* advance to next whether combined or incompatible */
+			i--;
 		}
 	}
 }
+
 
 /**
  * slide_line - reproduces a version of the "2048" game mechanics, on a single
@@ -120,20 +113,12 @@ int slide_line(int *line, size_t size, int direction)
 	/* screen invalid args */
 	if (line == NULL || size == 0 ||
 	    (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
-	{
-		fprintf(stderr, "slide_line: invalid args\n");
 		return (0);
-	}
 
 	/* reject arrays with any odd numbers */
 	for (i = 0; i < (int)size; i++)
-	{
 		if (line[i] % 2)
-		{
-			fprintf(stderr, "slide_line: odd number in array\n");
 			return (0);
-		}
-	}
 
 	/* array of 1 is already solved */
 	if ((int)size == 1)
