0cb37d732694e0407ebb9e2e216e589761abef8b
Samuel Pomeroy <1690@holbertonschool.com>
2021-07-05 17:03:45 +0000

    first push with setup 0x1B

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,19 @@
+#include "sort.h"
+
+/* malloc free */
+#include <stdlib.h>
+
+
+/**
+ * radix_sort - sorts an array of integers in ascending order using the (LSD)
+ *   Radix sort algorithm
+ *
+ * Note: while taking an array of signed type `int`, assumes that values in
+ *   `array` are all >= 0
+ * 
+ * @array:
+ * @size:
+ */
+void radix_sort(int *array, size_t size)
+{
+}
---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,7 @@
+#ifndef SORT_H
+#define SORT_H
+
+/* 0. Radix sort */
+void radix_sort(int *array, size_t size);
+
+#endif /* SORT_H */
