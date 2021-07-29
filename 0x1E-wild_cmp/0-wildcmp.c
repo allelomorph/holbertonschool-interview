318701f8dc83a7edabe82a4c10b050b1c7f88f46
Samuel Pomeroy <1690@holbertonschool.com>
2021-07-29 18:46:30 +0000

    0x1E t0 first working version, still needs refinement

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -12,6 +12,42 @@
  * - static variables are not allowed
  */
 /**
+ * globMatch - recursive helper to _wildcmp, checks substrings in base and
+ *   pattern to see if the local base substring is a valid match to the local
+ *   pattern substring following the current wildcard, or if the current
+ *   wildcard should continue to skip ahead in the base string
+ *
+ * @base: current position in base string for comparison
+ * @pattern: current postion in pattern string to compare to - can contain the
+ *   special character `*`, which can represent any string (including an empty
+ *   string)
+ *
+ * Return: 1 if glob matches base after current wildcard, 0 if not or failure
+ */
+int globMatch(char *base, char *pattern, int *tested_indicies)
+{
+	if (!base || !pattern || !tested_indicies)
+		return (0);
+/*
+	printf("\t\tglobMatch: base:'%s' pattern:'%s' *tested_indicies:%i\n", base, pattern, *tested_indicies);
+*/
+	(*tested_indicies)++;
+
+	/*
+	 * If glob is at end of pattern or is bounded by another wildcard,
+	 * then it counts as a match beyond the current wildcard.
+	 */
+	if (*(pattern + 1) == '\0' || *(pattern + 1) == '*')
+		return (1);
+
+	if (*base == *pattern)
+		return (globMatch(base + 1, pattern + 1, tested_indicies));
+
+	return (0);
+}
+
+
+/**
  * _wildcmp - recursive helper to wildcmp, compares two strings using
  *   UNIX-like asterisk wildcard syntax
  *
@@ -25,8 +61,52 @@
  */
 int _wildcmp(char *base, char *pattern, bool wildcard)
 {
+	int offset = 0;
+	int retval;
+
+	if (!base || !pattern)
+		return (0);
+/*
+	printf("\t_wildcmp: base:'%s' pattern:'%s' wildcard:%s\n", base, pattern, wildcard ? "true" : "false");
+*/
 	if (*pattern == '*')
 		return (_wildcmp(base, pattern + 1, true));
+
+	if (*pattern == '?')
+	{
+		if (*base == '\0')
+			return (0);
+
+		return (_wildcmp(base + 1, pattern + 1, false));
+	}
+
+	if (wildcard)
+	{
+		if (*base == '\0')
+		{
+			if (*pattern)
+				return (0);
+			else
+				return (1);
+		}
+
+		if (*base == *pattern)
+		{
+			retval = globMatch(base, pattern, &offset);
+/*
+			printf("\t\t\tglobMatch:%i offset:%i\n", retval, offset);
+*/
+			if (retval)
+			{
+			        return (_wildcmp(base + offset,
+						 pattern + offset, false));
+			}
+			return (_wildcmp(base + offset, pattern, true));
+		}
+
+		return (_wildcmp(base + 1, pattern, true));
+	}
+
 
 	if (*base == *pattern)
 	{
@@ -35,9 +115,6 @@ int _wildcmp(char *base, char *pattern, bool wildcard)
 
 		return (_wildcmp(base + 1, pattern + 1, false));
 	}
-
-	if (wildcard)
-		return (_wildcmp(base + 1, pattern, wildcard));
 
 	return (0);
 }
