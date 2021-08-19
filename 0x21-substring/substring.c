1fc6f1335e0361d7dfe56985700976b67ce54172
Samuel Pomeroy <1690@holbertonschool.com>
2021-08-19 18:06:52 +0000

    0x21 setup with empty files

---
+++ 0x21-substring/substring.c
@@ -0,0 +1,20 @@
+#include "substring.h"
+
+
+/**
+ * find_substring - Finds all the possible substrings containing a list of
+ *   words within a given string. A valid substring of `s` is the concatenation
+ *   of each word in `words` exactly once and without any intervening characters
+ *
+ * @s: string to scan
+ * @words: array of words all substrings must be a concatenation of to be
+ *   valid; all words in this array are the same length
+ * @nb_words: number of elements in the array words
+ * @n: holds the address at which to store the number of elements in the returned array.
+ *
+ * Return: an allocated array storing each index in s at which a substring was
+ *   found, or NULL on failure
+ */
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+}
